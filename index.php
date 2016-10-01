f000b248d42d05e7cab947695c5a0405bc12798f
Rosie <marta.deskiewicz@gmail.com>
2016-10-01 18:23:35 +0200

    adding links

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Welcome to my Web</title>
+<title>Welcome to my Website</title>
+<link rel="stylesheet" type="text/css" href='index_style.css'>
 </head>
 
 <body>
+
+<h1>Please login or register</h1>
+<a href="login.php">Login</a> or
+<a href="register.php">Register</a>
 </body>
 </html>
